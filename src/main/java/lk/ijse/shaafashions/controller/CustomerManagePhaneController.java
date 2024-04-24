1278f769b204b0548d8cc0bf52b02391c0a2d297
hirushar2003 <hirushaperera651@gmail.com>
2024-04-24 11:26:04 +0530

    Customer manage done

--- src/main/java/lk/ijse/shaafashions/controller/CustomerManagePhaneController.java
+++ src/main/java/lk/ijse/shaafashions/controller/CustomerManagePhaneController.java
@@ -1,8 +1,14 @@
 package lk.ijse.shaafashions.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.shaafashions.model.Customer;
+import lk.ijse.shaafashions.repository.CustomerRepo;
+
+import java.sql.SQLException;
 
 public class CustomerManagePhaneController {
     public AnchorPane customerManagePhane;
@@ -13,10 +19,63 @@ public class CustomerManagePhaneController {
     public TextField txtCustomerIdEnter;
 
     public void btnDeleteOnAction(ActionEvent actionEvent) {
+        String id = txtCustomerIdEnter.getText();
 
+        try {
+            Boolean isDeleted = CustomerRepo.delete(id);
+            if (isDeleted){
+                new Alert(Alert.AlertType.CONFIRMATION, "customer deleted succesfully").show();
+                clearFields();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
+        int id = Integer.parseInt(txtCustomerIdEnter.getText());
+        String name = txtCustomerNameManage.getText();
+        String address = txtCustomeAddresseManage.getText();
+        int contact = Integer.parseInt(txtCustomerContactManage.getText());
+        String email = txtCustomerEmailManage.getText();
 
+        Customer customer = new Customer(id, name, address, contact, email);
+
+        try {
+            boolean isUpdate = CustomerRepo.update(customer);
+            if (isUpdate){
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Updated Sucessfully").show();
+                clearFields();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    public void imgSearchOnAction(MouseEvent mouseEvent) throws SQLException {
+        String id = txtCustomerIdEnter.getText();
+
+        Customer customer = CustomerRepo.searchById(id);
+
+        if (customer != null){
+            txtCustomerNameManage.setText(customer.getName());
+            txtCustomeAddresseManage.setText(customer.getAddress());
+            txtCustomerContactManage.setText(String.valueOf(customer.getContact()));
+            txtCustomerEmailManage.setText(customer.getEmail());
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "Customer not found").show();
+        }
+    }
+
+    public void btnEditOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void clearFields (){
+        txtCustomerEmailManage.clear();
+        txtCustomerContactManage.clear();
+        txtCustomeAddresseManage.clear();
+        txtCustomerNameManage.clear();
+        txtCustomerIdEnter.clear();
     }
 }
--- src/main/java/lk/ijse/shaafashions/repository/CustomerRepo.java
+++ src/main/java/lk/ijse/shaafashions/repository/CustomerRepo.java
@@ -1,5 +1,6 @@
 package lk.ijse.shaafashions.repository;
 
+import javafx.scene.control.Alert;
 import lk.ijse.shaafashions.db.DbConnection;
 import lk.ijse.shaafashions.model.Customer;
 
@@ -46,5 +47,52 @@ public class CustomerRepo {
             customerList.add(customer);
         }
         return customerList;
+    }
+    public static Customer searchById (String id) throws SQLException {
+        String sql = "SELECT * FROM customers WHERE customerId = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setObject(1, id);
+
+        ResultSet resultSet = pstm.executeQuery();
+
+        if (resultSet.next()) {
+            int customerId = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String address = resultSet.getString(3);
+            int contact = resultSet.getInt(4);
+            String email = resultSet.getString(5);
+
+            Customer customer = new Customer(customerId, name, address, contact, email);
+
+            return customer;
+        }
+        return null ;
+    }
+    public static boolean delete (String id) throws SQLException {
+        String sql = "DELETE FROM customers WHERE customerId = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setObject(1, id);
+
+        return pstm.executeUpdate() > 0;
+    }
+
+    public static boolean update(Customer customer) throws SQLException {
+        String sql = "UPDATE customers SET name = ?, address = ?, contact = ?, email = ? WHERE customerId = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setObject(1, customer.getName());
+        pstm.setObject(2, customer.getAddress());
+        pstm.setObject(3, customer.getContact());
+        pstm.setObject(4, customer.getEmail());
+        pstm.setObject(5, customer.getCustomerId());
+
+        return pstm.executeUpdate() > 0;
     }
 }
